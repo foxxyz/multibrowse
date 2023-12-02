dd9605b164a9ae7ca07c58dfc46f3d4825371116
Ivo Janssen <foxxyz@gmail.com>
2023-12-01 22:41:46 -0800

    feat: linux support working

--- src/platforms/linux.rs
+++ src/platforms/linux.rs
@@ -1,10 +1,13 @@
 use std::process::Command;
+use crate::shared::Screen;
+use regex::Regex;
 
 const POSSIBLE_BROWSER_BINARIES: [&str; 3] = [
     "chromium-browser",    // ubuntu/debian
     "chromium",            // arch
     "google-chrome-stable" // arch
 ];
+
 pub fn browser_path() -> String {
     for binary in POSSIBLE_BROWSER_BINARIES {
         let which = Command::new("which")
@@ -12,8 +15,34 @@ pub fn browser_path() -> String {
             .output()
             .expect("unable to get browser location!");
         let location = String::from_utf8_lossy(&which.stdout);
-        //dbg!(location);
-        return location.trim_end_matches("\n").to_string();
+	let location = location.trim_end_matches("\n").to_string();
+        if location != "" {
+	    return location;
+	}
     }
     panic!("No suitable browser found to launch!");
+}
+
+pub fn displays() -> Vec<Screen> {
+    let mut connected: Vec<Screen> = Vec::new();
+    let xrandr = Command::new("xrandr")
+    	.output()
+	.expect("Please install xrandr to use multibrowse!");
+    let output = String::from_utf8_lossy(&xrandr.stdout);
+    let re = Regex::new(r" ([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)").unwrap();
+    let lines = output.lines();
+    for (id, line) in lines.enumerate() {
+        if !line.contains(" connected") {
+	    continue;
+	}
+    	let (_, [width, height, x, y]) = re.captures(line).unwrap().extract();
+	connected.push(Screen {
+	    id: id as u8,
+	    width: width.parse::<u32>().unwrap(),
+	    height: height.parse::<u32>().unwrap(),
+	    x: x.parse::<i32>().unwrap(),
+	    y: y.parse::<i32>().unwrap()
+	})
+    }
+    connected
 }
